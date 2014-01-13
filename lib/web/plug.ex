1324faad550d048feec6f9fa1f5b86c4737e30a0
Shane Logsdon <shane@shanelogsdon.com>
2014-01-13 00:27:56 -0500

    this is cleaner

--- lib/web/plug.ex
+++ lib/web/plug.ex
@@ -6,21 +6,16 @@ defmodule Web.Plug do
 
   def call(conn, []) do
     conn = case conn.path_info do
-      ["hello"] -> Module.function(@module, @action, 1).(conn)
+      ["hello"] -> apply(@module, @action, [conn])
       _ -> conn |> resp 200, "go away!"
     end
-
-    # conn = conn
-    #   |> put_resp_content_type("text/html")
-    #   |> resp(200, body)
-    #   |> send
 
     {:ok, conn |> send}
   end
 end
 
 defmodule Hello do
-  import Plug.Connection
+  use Controller
   def index(conn) do
     conn
       |> put_resp_content_type("text/html")
