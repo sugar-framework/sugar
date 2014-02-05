4e147a85f7bdc2770b277e86c6a0106b0cbaf8e8
Shane Logsdon <shane@shanelogsdon.com>
2014-02-05 13:26:01 -0500

    added render, halt! response helpers

--- lib/sugar/controller.ex
+++ lib/sugar/controller.ex
@@ -1,11 +1,21 @@
 # awesome example by @josevalim: https://gist.github.com/josevalim/7432084
 defmodule Sugar.Controller do
+  import Plug.Connection
+
   @doc """
   `__using__/1` macro used to add necessary items to a controller.
   """
   defmacro __using__(_) do
     quote do
-      import unquote(Plug.Connection)
+      import unquote(__MODULE__)
     end
+  end
+
+  def render(conn, _template) do
+    {:ok, send_resp(conn, 200, "hello world")}
+  end
+
+  def halt!(conn) do
+    {:ok, send_resp(conn, 401, "")}
   end
 end
