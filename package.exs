84f1ee021aa12e66ce8868671a270b6ede19c8bc
Shane Logsdon <shane@shanelogsdon.com>
2014-02-07 17:35:10 -0500

    updating package information

--- package.exs
+++ package.exs
@@ -1,6 +1,11 @@
-Expm.Package.new(name: "sugar", description: "Web framework for Elixir, built on top of Plug",
-                 version: "0.1.0", keywords: [], 
-                 maintainers: [[name: "Shane Logsdon", 
-                                email: "shane@logsdon.io"]],
-                 repositories: [[github: "slogsdon/sugar"]])
-    
+Expm.Package.new(
+  name: "sugar", 
+  description: "Web framework for Elixir, built on top of Plug",
+  homepage: "http://shane.logsdon.io/projects/sugar/",
+  version: "0.1.0", 
+  keywords: ["web", "framework", "plug"], 
+  licenses: [[name: "MIT"]],
+  maintainers: [[name: "Shane Logsdon", 
+                email: "shane@logsdon.io"]],
+  repositories: [[github: "slogsdon/sugar"]]
+)
