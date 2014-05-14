e37c1413db2a9b33940bb41938e8b156c0e5bbcb
Shane Logsdon <shane@shanelogsdon.com>
2014-05-14 00:55:43 -0400

    sugar.router not needed, but why not

--- test/sugar/router_test.exs
+++ test/sugar/router_test.exs
@@ -1,14 +1,96 @@
 defmodule Sugar.RouterTest do
   use ExUnit.Case, async: true
   use Plug.Test
+  use Sugar.Router
 
-  test "the truth" do
-    assert true
+  test "get/3" do
+    conn = conn(:get, "/get")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "post/3" do
+    conn = conn(:post, "/post")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "put/3" do
+    conn = conn(:put, "/put")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "patch/3" do
+    conn = conn(:patch, "/patch")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "delete/3" do
+    conn = conn(:delete, "/delete")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "options/3" do
+    conn = conn(:options, "/options")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "any/3" do
+    conn = conn(:get, "/any")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+
+    conn = conn(:any, "/any")
+    conn = Sugar.RouterTest.Router.call(conn, [])
+
+    assert conn.state === :sent
+    assert conn.status === 200
+  end
+
+  test "resource/2" do
+    assert true === true
   end
 
   defmodule Foo do
     use Sugar.Controller
-    def bar(conn) do
+
+    def get(conn, _args) do
+      render conn, ""
+    end
+    def post(conn, _args) do
+      render conn, ""
+    end
+    def put(conn, _args) do
+      render conn, ""
+    end
+    def patch(conn, _args) do
+      render conn, ""
+    end
+    def delete(conn, _args) do
+      render conn, ""
+    end
+    def options(conn, _args) do
+      render conn, ""
+    end
+    def any(conn, _args) do
       render conn, ""
     end
   end
@@ -16,6 +98,12 @@ defmodule Sugar.RouterTest do
   defmodule Router do
     use Sugar.Router
 
-    get "/", Foo, :bar
+    get      "/get",     Foo, :get
+    post     "/post",    Foo, :post
+    put      "/put",     Foo, :put
+    patch    "/patch",   Foo, :patch
+    delete   "/delete",  Foo, :delete
+    options  "/options", Foo, :options
+    any      "/any",     Foo, :any
   end
 end
