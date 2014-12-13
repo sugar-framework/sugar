bb74a7f0fb5f7425318cdcd90ea816c037625ba6
Viktor Justo Vasquez <viktor@vjustov.me>
2014-12-13 19:10:34 -0400

    Enables Sugar.View.Finder to find views when they don't have extension.

--- test/sugar/views/finder_test.exs
+++ test/sugar/views/finder_test.exs
@@ -29,4 +29,24 @@ defmodule Sugar.Views.FinderTest do
     assert template.engine === expected.engine
     assert template.source === expected.source
   end
+
+  test "one/2 without extension" do
+    template = Sugar.Views.Finder.one("test/fixtures/view_finder", "index")
+    expected = %Sugar.Templates.Template{
+                  engine: Sugar.Templates.Engines.EEx,
+                  key: "index.html.eex",
+                  source: ""
+                }
+
+    assert template.key === expected.key
+    assert template.engine === expected.engine
+    assert template.source === expected.source
+  end
+
+  test "one/2 when view does not exist" do
+    template = Sugar.Views.Finder.one("test/fixtures/view_finder", "not_found.html.eex")
+    expected = { :error, :notfound }
+
+    assert template === expected
+  end
 end
